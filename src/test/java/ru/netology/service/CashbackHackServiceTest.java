8a4cddb0da662771f433ddb75dff00eab9cdcacc
Uliana Le <ulianochka29@mail.ru>
2022-09-04 08:07:01 +0300

    add junit4-platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,8 @@
 package ru.netology.service;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -19,5 +20,20 @@ public class CashbackHackServiceTest {
     @Test
     public void test3() {
         Assert.assertEquals(0, service.remain(1000));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test4() {
+        Assertions.assertEquals(1, service.remain(999));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void test5() {
+        Assertions.assertEquals(999, service.remain(1001));
+    }
+
+    @Test
+    public void test6() {
+        Assertions.assertEquals(0, service.remain(1000));
     }
 }
